fcb838ea2a4214619e566ee5105889f45a437c6b
linkai <18337180580@163.com>
2022-10-20 18:38:25 +0800

    基本路由

--- routers/index.go
+++ routers/index.go
@@ -2,13 +2,18 @@ package routers
 
 import (
 	"github.com/gin-gonic/gin"
+	"goRedisAdmin/routers/api_router"
 	"goRedisAdmin/routers/view_router"
 	"log"
 )
 
 func RunApp() {
 	router := gin.New()
+	//渲染视图
 	router = view_router.RunViewRouter(router)
+	//API
+	api_router.RunApiRouter(router.Group(""))
+
 	err := router.Run(":9527")
 	if err != nil {
 		log.Fatalln(err)
