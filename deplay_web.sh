4bb1b26715836743bad12c5ccbea1644885757f9
linkai <738256016@qq.com>
2023-10-10 18:49:24 +0800

    添加web快速部署脚步

--- deplay_web.sh
+++ deplay_web.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # 目标路径
-target_path="/1panel/1panel/apps/openresty/uiucode-nginx/www/sites/gradmin.uiucode.com/index/web"
+target_path="/1panel/1panel/apps/openresty/uiucode-nginx/www/sites/gradmin.uiucode.com/index/web/"
 # 要复制的文件夹路径
-source_folder="./html"
+source_folder="./html/*"
 
 # 复制文件夹并覆盖目标文件夹
 cp -r -f "$source_folder" "$target_path"
